6c721d00e87292bcbc8c74e5a102d2ae4fc0380d
Abdulquadri31 <abdulquadriakinde@gmai.com>
2024-07-30 19:42:30 +0100

    first printf

--- main.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int _printf(const char *format, ...);
-
-int main(void)
-{
-	_printf("Hello, %s! This is a custom %c function. And here is a percent sign: %%\n", "world", 'C');
-	return 0;
-}
